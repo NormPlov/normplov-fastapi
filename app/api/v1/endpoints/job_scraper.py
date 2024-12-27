706fe79f723a5828d71fa8bd1a353d98bf7184d8
lymann <phplaravel576@gmail.com>
2024-12-27 16:17:37 +0700

    modify update scraping job method logic

--- app/api/v1/endpoints/job_scraper.py
+++ app/api/v1/endpoints/job_scraper.py
@@ -1,3 +1,4 @@
+import logging
 import httpx
 
 from fastapi import APIRouter, HTTPException, Depends
@@ -9,25 +10,35 @@ from app.schemas.job_scraper import ScrapeRequest
 job_scaper_router = APIRouter()
 
 DJANGO_BASE_URL = "http://136.228.158.126:3290/api/v1"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/token")
+logger = logging.getLogger(__name__)
 
 
 @job_scaper_router.get("/jobs", tags=["Django Jobs"])
-async def get_jobs(token: str = Depends(oauth2_scheme)):
+async def get_jobs(
+        current_user: User = Depends(is_admin_user),
+        token: str = Depends(oauth2_scheme)
+):
 
     headers = {"Authorization": f"Bearer {token}"}
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(verify=False, timeout=700.0) as client:
         try:
             response = await client.get(f"{DJANGO_BASE_URL}/jobs", headers=headers)
             response.raise_for_status()
             return response.json()
-        except httpx.HTTPError as e:
-            raise HTTPException(status_code=500, detail=f"Failed to fetch jobs: {e}")
+        except httpx.RequestError as e:
+            raise HTTPException(status_code=500, detail=f"Request error: {e}")
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(status_code=e.response.status_code, detail=f"HTTP error: {e}")
 
 
 @job_scaper_router.get("/jobs/{uuid}", tags=["Django Jobs"])
-async def get_job_details(uuid: str, token: str = Depends(oauth2_scheme)):
+async def get_job_details(
+        uuid: str,
+        current_user: User = Depends(is_admin_user),
+        token: str = Depends(oauth2_scheme)
+):
     headers = {"Authorization": f"Bearer {token}"}
     async with httpx.AsyncClient() as client:
         try:
@@ -70,21 +81,34 @@ async def trigger_job_scraper(
 
 
 @job_scaper_router.patch("/jobs/update/{uuid}", tags=["Django Jobs"])
-async def update_job(uuid: str, job_data: dict, token: str = Depends(oauth2_scheme)):
+async def update_job(
+    uuid: str,
+    job_data: dict,
+    current_user: User = Depends(is_admin_user),
+    token: str = Depends(oauth2_scheme),
+):
     headers = {"Authorization": f"Bearer {token}"}
+
     async with httpx.AsyncClient(timeout=300.0) as client:
         try:
-            response = await client.patch(f"{DJANGO_BASE_URL}/jobs/update/{uuid}", json=job_data, headers=headers)
-            print(f"Request Payload: {job_data}")
-            print(f"Request Headers: {headers}")
+            response = await client.patch(
+                f"{DJANGO_BASE_URL}/jobs/update/{uuid}",
+                json=job_data,
+                headers=headers,
+            )
             response.raise_for_status()
             return response.json()
         except httpx.HTTPError as e:
+            logger.error(f"HTTPError: {e}")
             raise HTTPException(status_code=500, detail=f"Failed to update job: {e}")
 
 
 @job_scaper_router.delete("/jobs/delete/{uuid}", tags=["Django Jobs"])
-async def delete_job(uuid: str, token: str = Depends(oauth2_scheme)):
+async def delete_job(
+        uuid: str,
+        current_user: User = Depends(is_admin_user),
+        token: str = Depends(oauth2_scheme)
+):
     headers = {"Authorization": f"Bearer {token}"}
     async with httpx.AsyncClient() as client:
         try:
