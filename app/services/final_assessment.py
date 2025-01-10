e5f8e9ec5347c765c1ff39a2869a0a71ab806e38
lymann <phplaravel576@gmail.com>
2025-01-10 14:40:32 +0700

    update

--- app/services/final_assessment.py
+++ app/services/final_assessment.py
@@ -44,10 +44,7 @@ async def predict_careers_service(
 
         user_input = prepare_model_input(aggregated_response)
 
-        dataset_path = os.path.join(
-            os.getcwd(),
-            r"D:\CSTAD Scholarship Program\python for data analytics\NORMPLOV_PROJECT\normplov-fastapi\datasets\train_testing.csv",
-        )
+        dataset_path = "/app/datasets/train_testing.csv"
 
         career_model = load_career_recommendation_model(dataset_path=dataset_path)
         model_features = career_model.get_feature_columns()
